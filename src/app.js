b88d787822f36e15fe6e76f7bb284df2e7cc6097
frncsdrk <git@frncsdrk.me>
2024-08-06 21:26:25 +0200

    [feat] Implement configurable base path

--- src/app.js
+++ src/app.js
@@ -12,20 +12,22 @@ const views = require('./routes/views');
 start = () => {
   const app = express();
 
+  const basePath = config.get('service.server.path').replace(/\/$/, '');;
+
   app.use(express.json());
   app.use(express.text({ limit: '50mb', type: 'text/*' }));
   app.use(express.urlencoded({ extended: false }));
   app.use(cors());
 
-  app.use('/static', express.static('public'));
+  app.use(basePath + '/static', express.static('public'));
 
-  app.set("view engine", "pug");
-  app.set("views", path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
+  app.set('views', path.join(__dirname, 'views'));
 
-  app.use('/api/healthz', healthz);
-  app.use('/api/image', image);
-  app.use('/api/video', video);
-  app.use('/', views);
+  app.use(basePath + '/api/healthz', healthz);
+  app.use(basePath + '/api/image', image);
+  app.use(basePath + '/api/video', video);
+  app.use(basePath + '/', views);
 
   server = app.listen(process.env.LENSCAST_PORT || config.get('service.server.port'), () => {
     console.log('app is running on', server.address().port);
