23079896cc7eb462b11504b2da82f9eea81fc944
frncsdrk <git@frncsdrk.me>
2024-10-27 22:15:35 +0100

    [feat] Implement log rotation
    - fix console logging format

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,20 @@
+const winston = require('winston');
 const { createLogger, format, transports } = require('winston');
+require('winston-daily-rotate-file');
+
+const fileRotateCombinedTransport = new winston.transports.DailyRotateFile({
+  filename: 'log/combined-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxFiles: '14d',
+  zippedArchive: true,
+});
+const fileRotateErrorTransport = new winston.transports.DailyRotateFile({
+  level: 'error',
+  filename: 'log/error-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxFiles: '14d',
+  zippedArchive: true,
+});
 
 const logger = createLogger({
   level: 'info',
@@ -13,12 +29,13 @@ const logger = createLogger({
   defaultMeta: { service: 'lenscast' },
   transports: [
     //
-    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-    // - Write all logs error (and below) to `quick-start-error.log`.
+    // - Write to all logs with level `info` and below to `combined.log`.
+    // - Write all logs error (and below) to `error.log`.
     //
-    new transports.File({ filename: 'log/error.log', level: 'error' }),
-    new transports.File({ filename: 'log/combined.log' }),
-  ]
+    fileRotateCombinedTransport,
+    fileRotateErrorTransport
+  ],
+  exitOnError: false,
 });
 
 //
@@ -27,10 +44,7 @@ const logger = createLogger({
 //
 if (process.env.NODE_ENV !== 'prod') {
   logger.add(new transports.Console({
-    format: format.combine(
-      format.colorize(),
-      format.simple(),
-    )
+    format: format.cli(),
   }));
 }
 
